df9aac5b18f76c97caff59490bf7a6b05b8c4db9
Tyler Cardon <tyler.k.cardon@gmail.com>
2017-02-01 02:44:29 -0600

    Use PORT env variable on Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function(request, response){
     response.sendFile(__dirname+'/index.html');
 });
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function(req,res) {
     console.log("Listening to http://localhost:" + port);
