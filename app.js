3ccaa3cdf61cabfd32a9996aa9b3cfe7adea54df
Tyler Cardon <tyler.k.cardon@gmail.com>
2017-02-01 02:30:15 -0600

    Heroku hosted node.js project

---
+++ app.js
@@ -0,0 +1,12 @@
+var express=require('express');
+var app=express();
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname+'/index.html');
+});
+
+var port = process.env.port || 8080;
+
+var server = app.listen(port, function(req,res) {
+    console.log("Listening to http://localhost:" + port);
+});
